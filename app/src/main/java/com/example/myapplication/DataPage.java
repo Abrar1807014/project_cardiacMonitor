01c609178c3903f52f994a79b172658e3288f530
Abrar1807014 <fiyaz1807014@stud.kuet.ac.bd>
2022-06-28 21:06:43 +0600

    DataAdd, DataSaver, Records added

--- app/src/main/java/com/example/myapplication/DataPage.java
+++ app/src/main/java/com/example/myapplication/DataPage.java
@@ -2,13 +2,39 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DataPage extends AppCompatActivity {
+    Button button;
+    Button floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_page);
+
+        button=findViewById(R.id.records);
+        floatingActionButton=findViewById(R.id.add);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DataPage.this, Records.class);
+                startActivity(intent);
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DataPage.this, DataAdd.class);
+                startActivity(intent);
+            }
+        });
     }
 }
